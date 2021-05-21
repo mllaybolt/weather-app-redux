487aaabebc0e18a92ea40ee23aa9c3f8d8fa9bb0
mllaybolt <81402055+mllaybolt@users.noreply.github.com>
2021-05-20 22:37:16 -0500

    Added JavaScript + weather API + axios
    Also wrote JS for automatic date & time display

---
+++ src/app.js
@@ -0,0 +1,61 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let day = days[date.getDay()];
+  let month = months[date.getMonth()];
+  let numericalDate = date.getDate();
+  let year = date.getFullYear();
+  return `🗓${day}, ${month} ${numericalDate}, ${year} 🕖${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
+
+let apiKey = "66e11cc33b0837aff2265fadb13ac0ad";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Winnipeg&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
